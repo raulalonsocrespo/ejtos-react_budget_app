b7e13e4230ec7175a133fabc3aede583a6892b92
raulalonsocrespo <raul@alonsocrespo.com>
2024-01-03 12:17:21 -0500

    Changes made on version 7

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -2,32 +2,19 @@ import React, { useContext, useState } from 'react';
 
 const Currency = () => {
 
-    const currencyBox = {
-        backgroundColor: 'rgb(147, 228, 153)',
-        borderBottom: '1vh solid white',
-        color: 'white',
-        paddingLeft: '3vh',
-        paddingRight: '3vh',
-        paddingTop: '1vh',
-        paddingBottom: '0vh',
-        fontSize: '1.2rem',
-        borderRadius: '0.375rem',
-        maxWidth: '75%',
-        heigt: '37px'
-    };
-
+    
     const currencySelector = {
         backgroundColor: 'rgb(147, 228, 153)',
-        borderTop: 'none',
-        borderRight: 'none',
-        borderLeft: 'none',
-        borderBottom: '1vh solid white',
+        borderTop: '1px solid white',
+        borderRight: '3px solid white',
+        borderLeft: '3px solid white',
+        borderBottom: '3px solid white',
         color: 'white',
-        padding: '2vh 2vh 3vh 2vh',
+        padding: '2vh 2vh 2vh 2vh',
         fontSize: '1.2rem',
-        borderRadius: '0.375rem',
+        borderRadius: '0.55rem',
         Width: '70%',
-        texAlign: 'center',
+        texAlign: 'center'
     };
 
     const currencyOpt = {
@@ -40,7 +27,7 @@ const Currency = () => {
     return (
         <div className='currencyHeader'>
             <select id="currency" style={currencySelector}>
-                <option value="$" style={currencyOpt}><span hidden>Currency (</span>$ Dollar<span hidden>)</span></option>
+                <option value="$" style={currencyOpt}><span>Currency (</span>$ Dollar<span hidden>)</span></option>
                 <option value="£" style={currencyOpt} selected><span hidden>Currency (</span>£ Pound<span hidden>)</span></option>
                 <option value="€" style={currencyOpt}><span hidden>Currency (</span>€ Euro<span hidden>)</span></option>
                 <option value="₹" style={currencyOpt}><span hidden>Currency (</span>₹ Ruppee<span hidden>)</span></option>
