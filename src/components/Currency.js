48a426289be79736eb2d6f591dc9e396110b8413
raulalonsocrespo <raul@alonsocrespo.com>
2024-01-04 10:06:40 -0500

    Changes made on version 9

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,50 +1,58 @@
 import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Currency = () => {
 
     const currencyBox = {
         backgroundColor: 'rgb(147, 228, 153)',
         color: 'white',
-        padding: '0vh 0vw 0vh 1vw',
         fontSize: '1.2rem',
         borderRadius: '0.375rem',
-        width: '75%',
-        texAlign: 'center',
+        maxWidth: '100%',
         borderBottom: '3px solid white',
+        padding: '0vh 2vh 0vh 2vh'
     };
     
     const currencySelector = {
         backgroundColor: 'rgb(147, 228, 153)',
-        borderBottom: '3px solid white',
         color: 'white',
-        padding: '2vh 0vh 2vh 0vh',
-        texAlign: 'center'
+        border: 'none',
+        padding: '0vh 0vh 0vh 0vh',
+        textAlign: 'center',
+        display: 'initial'
     };
-
-    const currencyOpt = {
+   
+    let currencyOpt = {
         minHeight: '4vh',
         border: 'none',
         color: 'black',
-        backgroundColor: 'rgb(147, 228, 153)'
+        backgroundColor: 'rgb(147, 228, 153)',
+        width: '2rem',
+        textAlign: 'left'
+    };
+
+    const changeBackgroundFormat = (event) => {
+        
     };
 
     const [currency, setCurrency] = useState('£ Pound');
 
     const changeCurrency = (value) =>{
         setCurrency(value);
-        return <h1>Currency ({currency})</h1>
+        document.getElementById('currency').value = ""
     };
 
     return (
         <div className='currencyHeader' style={currencyBox}>
-            <span>Currency (</span>
-            <select id="currency" style={currencySelector} onClick={(event) => changeCurrency(event.target.value)} >
-                <option value="$" style={currencyOpt}>$ Dollar</option>
-                <option value="£" style={currencyOpt} selected>£ Pound</option>
-                <option value="€" style={currencyOpt}>€ Euro</option>
-                <option value="₹" style={currencyOpt}>₹ Ruppee</option>
+            <span>Currency ({currency})</span>
+            <div>
+            <select id="currency" style={currencySelector} onChange={(event) => changeCurrency(event.target.value)} onMouseOver={changeBackgroundFormat}>
+                <option value="$ Dollar" style={currencyOpt}>$ Dollar</option>
+                <option value="£ Pound" style={currencyOpt}>£ Pound</option>
+                <option value="€ Euro" style={currencyOpt}>€ Euro</option>
+                <option value="₹ Ruppee" style={currencyOpt}>₹ Ruppee</option>
             </select>
-            <span>)</span>
+            </div>
         </div>
     );
 };
